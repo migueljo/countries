f408b4aef187d8798183bd9fc946ad1d09f67313
Miguel Martínez <mmartinez@livevox.com>
2020-10-11 11:29:19 -0500

    Showing no countries found text

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -6,15 +6,19 @@ import * as Styles from './CountriesStyles'
 export default function Countries({ countries }) {
   return (
     <Styles.Container>
-      {countries.map(country => (
-        <Styles.Card
-          to={`/country/${country.alpha3Code.toLowerCase()}`}
-          key={country.alpha3Code}
-          style={{ textDecoration: 'none', color: 'black' }}
-        >
-          <CountryCard country={country} />
-        </Styles.Card>
-      ))}
+      { countries.length > 0 ? (
+        countries.map(country => (
+          <Styles.Card
+            to={`/country/${country.alpha3Code.toLowerCase()}`}
+            key={country.alpha3Code}
+            style={{ textDecoration: 'none', color: 'black' }}
+          >
+            <CountryCard country={country} />
+          </Styles.Card>
+        ))
+      ) : (
+        <p>No countries found</p>
+      )}
     </Styles.Container>
   )
 }
